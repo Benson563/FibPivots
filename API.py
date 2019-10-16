38ad0322a227d383d3a4b037b6fffde8cadfc263
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-16 02:10:49 -0500

    API

---
+++ API.py
@@ -0,0 +1,12 @@
+# API Key for Alpha Vantage: D1UA2DG18SPT7MCN
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+
+key = 'D1UA2DG18SPT7MCN'
+ts = TimeSeries(key=key, output_format='pandas')
+data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
+print(data)
+
+
