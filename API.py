7542d66498a6a87960bf6ee554a6c946e393b4ed
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-27 00:07:29 -0500

    finished with weeklyPivotInfo for now. implemented arg for all methods

--- API.py
+++ API.py
@@ -6,15 +6,19 @@ import time
 key = 'D1UA2DG18SPT7MCN'
 
 
-def dailyPivotInfo():  # gotta figure out how to parse of the daily info..
+def dailyPivotInfo(symbol):  # gotta figure out how to parse of the daily info..
     ts = TimeSeries(key=key, output_format='pandas')
-    data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
-    print((data.head(1)))  # gives off the data of the last trading day
+    data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
+    print(data.head(1))  # gives off the data of the last trading day
 
 
-def intradayInfo(): #still gotta figure out how to parse
+def weeklyPivotInfo(symbol):
     ts = TimeSeries(key=key, output_format='pandas')
-    data, meta_data = ts.get_quote_endpoint(symbol='SPY')
+    data, meta_data = ts.get_weekly(symbol=symbol)
+    print(data.head(1)) #gets the last weeks trading info
+
+
+def stockQuote(symbol):  # still gotta figure out how to parse
+    ts = TimeSeries(key=key, output_format='pandas')
+    data, meta_data = ts.get_quote_endpoint(symbol=symbol)
     print(data)
-
-
