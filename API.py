286f8b58bab22155173768062021678ea9952717
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-26 23:59:44 -0500

    finished intradayInfo for now

--- API.py
+++ API.py
@@ -3,13 +3,18 @@ import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 import time
 
+key = 'D1UA2DG18SPT7MCN'
 
-def dailyPivotInfo(): #gotta figure out how to parse of the daily info..
-    key = 'D1UA2DG18SPT7MCN'
+
+def dailyPivotInfo():  # gotta figure out how to parse of the daily info..
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
-    print((data.head(1))) #gives off the data of the last trading day
+    print((data.head(1)))  # gives off the data of the last trading day
 
 
+def intradayInfo(): #still gotta figure out how to parse
+    ts = TimeSeries(key=key, output_format='pandas')
+    data, meta_data = ts.get_quote_endpoint(symbol='SPY')
+    print(data)
 
-def intradayInfo():
+
