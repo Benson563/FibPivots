2e1eb0ff3b988446f8f174870cc897338c932b87
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-27 15:17:26 -0500

    got parsed info on dailyPivotInfo

--- API.py
+++ API.py
@@ -9,13 +9,25 @@ key = 'D1UA2DG18SPT7MCN'
 def dailyPivotInfo(symbol):  # gotta figure out how to parse of the daily info..
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
-    print(data.head(1))  # gives off the data of the last trading day
+    high = data.iloc[0,1]  # ['1. open', '2. high', '3. low', '4. close', '5. volume']
+    low = data.iloc[0,2]
+    close = data.iloc[0,3]
+
+
+
+
 
 
 def weeklyPivotInfo(symbol):
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_weekly(symbol=symbol)
-    print(data.head(1)) #gets the last weeks trading info
+    print(data.head(1))  # gets the last weeks trading info
+
+
+def monthlyPivotInfo(symbol):
+    ts = TimeSeries(key=key, output_format='pandas')
+    data, meta_data = ts.get_monthly(symbol=symbol)
+    print(data.head(1))
 
 
 def stockQuote(symbol):  # still gotta figure out how to parse
