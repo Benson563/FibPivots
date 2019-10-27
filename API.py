f2cd3f102452ff4f367bcf85459aaad866802dcb
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-27 15:51:40 -0500

    pivotPoint calculation

--- API.py
+++ API.py
@@ -9,28 +9,48 @@ key = 'D1UA2DG18SPT7MCN'
 def dailyPivotInfo(symbol):  # gotta figure out how to parse of the daily info..
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_daily(symbol=symbol, outputsize='full')
-    high = data.iloc[0,1]  # ['1. open', '2. high', '3. low', '4. close', '5. volume']
-    low = data.iloc[0,2]
-    close = data.iloc[0,3]
-
-
-
-
-
+    high = data.iloc[0, 1]  # ['1. open', '2. high', '3. low', '4. close', '5. volume']
+    low = data.iloc[0, 2]
+    close = data.iloc[0, 3]
+    pivotPointCalculation = (high + low + close) / 3
+    pivotPoint = round(pivotPointCalculation, 2)
+    print(pivotPoint)
+    return pivotPoint
 
 def weeklyPivotInfo(symbol):
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_weekly(symbol=symbol)
-    print(data.head(1))  # gets the last weeks trading info
+    high = data.iloc[1, 1]  # ['1. open', '2. high', '3. low', '4. close', '5. volume']
+    low = data.iloc[1, 2]
+    close = data.iloc[1, 3]
+    pivotPointCalculation = (high + low + close) / 3
+    pivotPoint = round(pivotPointCalculation, 2)
+    print(pivotPoint)
+    return pivotPoint
+
+
+#weeklyPivotInfo('amd')
 
 
 def monthlyPivotInfo(symbol):
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_monthly(symbol=symbol)
-    print(data.head(1))
+    high = data.iloc[1, 1]  # ['1. open', '2. high', '3. low', '4. close', '5. volume']
+    low = data.iloc[1, 2]
+    close = data.iloc[1, 3]
+    pivotPointCalculation = (high + low + close) / 3
+    pivotPoint = round(pivotPointCalculation, 2)
+    print(pivotPoint)
+    return pivotPoint
 
+
+# print(monthlyPivotInfo('amd'))
+# print('weekly info')
+# print(weeklyPivotInfo('amd'))
 
 def stockQuote(symbol):  # still gotta figure out how to parse
     ts = TimeSeries(key=key, output_format='pandas')
     data, meta_data = ts.get_quote_endpoint(symbol=symbol)
     print(data)
+
+# stockQuote('amd')
