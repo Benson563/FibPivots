36b39d4549f8ce97867b1c172d329a38f7d5c6e0
Nguyen, Benson <benson-nguyen@uiowa.edu>
2019-10-26 23:48:02 -0500

    added dailyPivotInfo method and working on intradayInfo Need to do MonthlyPivot and WeeklyPivot

--- API.py
+++ API.py
@@ -4,10 +4,12 @@ from alpha_vantage.timeseries import TimeSeries
 import time
 
 
-key = 'D1UA2DG18SPT7MCN'
-ts = TimeSeries(key=key, output_format='pandas')
-data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
-print(data)
-print(ts)
+def dailyPivotInfo(): #gotta figure out how to parse of the daily info..
+    key = 'D1UA2DG18SPT7MCN'
+    ts = TimeSeries(key=key, output_format='pandas')
+    data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
+    print((data.head(1))) #gives off the data of the last trading day
 
 
+
+def intradayInfo():
